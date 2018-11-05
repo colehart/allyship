2b4ac9b21f5a9a486886455bef794f7f21d8e10e
Cole Hart <cole@colehart.com>
2018-11-05 16:20:57 -0700

    Write and pass test for receivedError action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const isLoading = (bool) => ({
   type: 'IS_LOADING',
   isLoading: bool,
 })
+
+export const receivedError = (message) => ({
+  type: 'RECEIVED_ERROR',
+  receivedError: message,
+})
