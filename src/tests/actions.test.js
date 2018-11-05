6020a3e6632a5bfb3dff596d9f836f03ed823b94
Cole Hart <cole@colehart.com>
2018-11-05 16:19:54 -0700

    Add isLoading test and pass

--- src/tests/actions.test.js
+++ src/tests/actions.test.js
@@ -3,13 +3,24 @@ import { mockStories } from './testMocks'
 
 describe('actions', () => {
   it('should have a type of IS_LOADING', () => {
-    const stories = mockStories;
+    const expectedAction = {
+      type: 'IS_LOADING',
+      isLoading: true
+    }
 
+    const result = Actions.isLoading(true);
+    expect(result).toEqual(expectedAction)
   })
 
-  it('should have a type of SET_STORIES', () => {
-    const stories = mockStories;
+  it('should have a type of RECEIVED_ERROR', () => {
+    const mockErrorMessage = 'Your API key has been disabled.'
+    const expectedAction = {
+      type: 'RECEIVED_ERROR',
+      receivedError: mockErrorMessage,
+    }
 
+    const result = Actions.receivedError(mockErrorMessage)
+    expect(result).toEqual(expectedAction)
   })
 
   it('should have a type of RECEIVED_ERROR', () => {
